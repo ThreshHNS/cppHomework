1f731ad9abf8b8a6de04f1ec09c0419d00a0bef1
Andrey Velts <herovelts1@gmail.com>
2020-07-07 18:20:08 +0800

    Add files via upload

--- lab4/src/computer_interface.cpp
+++ lab4/src/computer_interface.cpp
@@ -4,33 +4,35 @@
 
 computer_interface_t::computer_interface_t() : win_counter(), lose_counter(), tie_counter() {}
 
-void computer_interface_t::on_incorrect_select_step(const step_t &step) const {
+computer_interface_t::~computer_interface_t() {}
+
+void computer_interface_t::on_incorrect_select_step(const step_t& step) const {
 	throw 400;
 }
 
-void computer_interface_t::on_incorrect_move_step(const step_t &step) const {
+void computer_interface_t::on_incorrect_move_step(const step_t& step) const {
 	throw 400;
 }
 
-void computer_interface_t::on_incorrect_attack_step(const step_t &step) const {
+void computer_interface_t::on_incorrect_attack_step(const step_t& step) const {
 	throw 400;
 }
 
 void computer_interface_t::on_win() {
-    win_counter++;
+	win_counter++;
 }
 
 void computer_interface_t::on_lose() {
-    lose_counter++;
+	lose_counter++;
 }
 
 void computer_interface_t::on_tie() {
-    tie_counter++;
+	tie_counter++;
 }
 
 void computer_interface_t::print_stat() const {
-    std::cout << "Wins:  " << win_counter << std::endl;
-    std::cout << "Loses: " << lose_counter << std::endl;
-    std::cout << "Ties:  " << tie_counter << std::endl;
-    std::cout << std::endl;
+	std::cout << "Wins:  " << win_counter << std::endl;
+	std::cout << "Loses: " << lose_counter << std::endl;
+	std::cout << "Ties:  " << tie_counter << std::endl;
+	std::cout << std::endl;
 }
